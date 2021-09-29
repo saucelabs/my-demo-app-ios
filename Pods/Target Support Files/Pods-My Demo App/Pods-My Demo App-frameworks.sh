f34abbd837a45793341b66394af478cc94c52ca5
Rizwan Ullah <jeerizwan@gmail.com>
2021-09-29 10:55:29 +0500

    Initial Commit

--- Pods/Target Support Files/Pods-My Demo App/Pods-My Demo App-frameworks.sh
+++ Pods/Target Support Files/Pods-My Demo App/Pods-My Demo App-frameworks.sh
@@ -176,9 +176,11 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/EasyTipView/EasyTipView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FormTextField/FormTextField.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/EasyTipView/EasyTipView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FormTextField/FormTextField.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
--- README.md
+++
@@ -1,1 +0,0 @@
-# my-demo-app-ios
